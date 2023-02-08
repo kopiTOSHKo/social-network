11452c07574898b59ee932f39c52a98fd21958c2
kopiTOSHKo <jezia1992@gmail.com>
2023-02-08 17:19:08 +0200

    Route all pages. 19 lesson.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
   return (
@@ -15,15 +18,17 @@ const App = () => {
         <Navbar />
         <div className='app-wrapper-content'>
           <Routes>
-            <Route path='/profile' element={Profile} />
-            <Route path='/dialogs' element={Dialogs} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/dialogs' element={<Dialogs />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/settings' element={<Settings />} />
           </Routes>
         </div>
       </div>
     </BrowserRouter>
   );
 }
-
 
 export default App;
 

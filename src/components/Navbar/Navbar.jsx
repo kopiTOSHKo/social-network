77de0cdf376f591fd08118d35c8d2eac9f8c3277
kopiTOSHKo <jezia1992@gmail.com>
2023-02-14 13:25:29 +0200

    NavLink class:'active'

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
@@ -5,19 +6,19 @@ const Navbar = () => {
         <nav className={classes.nav}>
             <ul>
                 <li className={classes.item}>
-                    <a href="/profile">Profile</a>
+                    <NavLink to="/profile" className={navData=>navData.isActive? classes.activeLink: classes.item}>Profile</NavLink>
                 </li>
                 <li className={classes.item}>
-                    <a href="/dialogs">Messages</a>
+                    <NavLink to="/dialogs" className={navData=>navData.isActive? classes.activeLink: classes.item}>Messages</NavLink>
                 </li>
                 <li className={classes.item}>
-                    <a href="/news">News</a>
+                    <NavLink to="/news" className={navData=>navData.isActive? classes.activeLink: classes.item}>News</NavLink>
                 </li>
                 <li className={classes.item}>
-                    <a href="/music">Music</a>
+                    <NavLink to="/music" className={navData=>navData.isActive? classes.activeLink: classes.item}>Music</NavLink>
                 </li>
                 <li className={classes.item}>
-                    <a href="/settings">Settings</a>
+                    <NavLink to="/settings" className={navData=>navData.isActive? classes.activeLink: classes.item}>Settings</NavLink>
                 </li>
             </ul>
         </nav>

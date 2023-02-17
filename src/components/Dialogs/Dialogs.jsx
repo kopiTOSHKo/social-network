377c15bd707d492afea8a931e4357639b8c691a0
kopiTOSHKo <jezia1992@gmail.com>
2023-02-17 14:50:29 +0200

    dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,28 @@
 import classes from './Dialogs.module.css'
 
-const Dialogs  = () => {
+const Dialogs = () => {
     return (
-    <div>
-        I am
-    </div>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Valera
+                </div>
+                <div className={classes.dialog}>
+                    Sergei
+                </div>
+                <div className={classes.dialog}>
+                    Dmitri
+                </div>
+                <div className={classes.dialog}>
+                    Olga
+                </div>
+            </div>
+            <div className={classes.massages}>
+                <div className={classes.massage}>Hi!</div>
+                <div className={classes.massage}>My name is...</div>
+                <div className={classes.massage}>Where are you from?</div>
+            </div>
+        </div>
     )
 }
 export default Dialogs;
